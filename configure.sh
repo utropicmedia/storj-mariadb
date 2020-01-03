ea41270cc16153234ef87ec07bddb36e3d9bec78
Karl Mozurkewich <karl@utropicmedia.com>
2020-01-02 23:38:01 -0500

    updated strcture

--- configure.sh
+++
@@ -1,7 +0,0 @@
-# create module path, if it does not exist
-mkdir -p $HOME/go/src/utropicmedia/maria_storj_interface/maria
-mkdir -p $HOME/go/src/utropicmedia/maria_storj_interface/storj
-# move go packages to include path
-cp utropicmedia/maria_storj_interface/maria/* $HOME/go/src/utropicmedia/maria_storj_interface/maria/
-cp utropicmedia/maria_storj_interface/storj/* $HOME/go/src/utropicmedia/maria_storj_interface/storj/
-cp utropicmedia/maria_storj_interface/* $HOME/go/src/utropicmedia/maria_storj_interface/
